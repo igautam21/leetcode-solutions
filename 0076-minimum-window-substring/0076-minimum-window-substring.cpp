ce87314e2455b4131a26e1f9e8b9608f321e430f
igautam21 <gy977078@gmail.com>
2026-06-28 08:46:20 +0530

    Time: 19 ms (23.35%), Space: 11.4 MB (64.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -7,17 +7,19 @@ public:
         }
         int l=0,r=0,c=0,minlength=INT_MAX,sidx=-1;
         while(r<s.length()){
-            if(mpp[s[r]]>0){
-                c++;
+            if(mpp.find(s[r])!=mpp.end()){
+                if(mpp[s[r]]>0) c++;
+                mpp[s[r]]--;
             }
-            mpp[s[r]]--;
             while(c==t.length()){
                 if((r-l+1)<minlength){
                     minlength=(r-l+1);
                     sidx=l;
                 }
-                mpp[s[l]]++;
-                if(mpp[s[l]]>0) c--;
+                if(mpp.find(s[l])!=mpp.end()){
+                    mpp[s[l]]++;
+                    if(mpp[s[l]]>0) c--;
+                }
                 l++;
             }
             r++;
