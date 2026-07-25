0c4accd3ef0571df4df4a378abd0f83a7747a76f
igautam21 <gy977078@gmail.com>
2026-07-25 11:57:25 +0530

    Time: 0 ms (100%), Space: 9.3 MB (10.11%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int ans=0;
+        string s=to_string(n);
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<s.length();j++){
+                ans=max(ans,(s[i]-'0')*(s[j]-'0'));
+            }
+        }
+        return ans;
+    }
+};
