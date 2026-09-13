e5b81dab7b39dd36109d8417fd75b27fdbea97ee
igautam21 <gy977078@gmail.com>
2026-09-13 11:24:42 +0530

    Time: 7 ms (27.69%), Space: 18.7 MB (39.9%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,24 +10,24 @@
  */
 class Solution {
 public:
-    ListNode *merge2Lists(ListNode *l1,ListNode *l2){
-        if(!l1) return l2;
-        if(!l2) return l1;
-        if(l1->val < l2->val){
-            l1->next=merge2Lists(l1->next,l2);
-            return l1;
-        }
-        else{
-            l2->next=merge2Lists(l1,l2->next);
-            return l2;
-        }
-    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.empty()) return nullptr;
-        ListNode *head=lists[0];
-        for(int i=1;i<lists.size();i++){
-            head=merge2Lists(head,lists[i]);
+        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>minh;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]!=nullptr){
+                minh.push({lists[i]->val,lists[i]});
+            }
         }
-        return head;
+        ListNode *ans=new ListNode(-1);
+        ListNode *temp=ans;
+        while(!minh.empty()){
+            ListNode *node=minh.top().second;
+            minh.pop();
+            temp->next=node;
+            temp=temp->next;
+            if(node->next!=NULL){
+                minh.push({node->next->val,node->next});
+            }
+        }
+        return ans->next;
     }
 };
